91c7e12f1bd94eb34e541e67b6a5d3854c2881e7
Juliana Amoasei <juliana.amoasei@gmail.com>
2020-12-03 00:27:48 -0300

    video 3.1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 const typeDefs = gql`
- type User {
-   nome: String
- }
- type Query {
-   users: [User]
- }
+  scalar DateTime
+  type User {
+    id: Int
+    nome: String
+    email: String
+    createdAt: DateTime
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int
+    titulo: String
+    conteudo: String
+  }
+
+  type Query {
+    users: [User]
+  }
 `
 
 const resolvers = {
  Query: {
-   users: () => [{ nome: "Ana" }, {nome: "Bia" }]
+   users: async () => await prisma.user.findMany({
+     include: {
+       posts: true
+     }
+   })
  }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, context: prisma })
 server.listen({ port: 4000 }, () => console.log(`Servidor pronto em localhost:4000`)) 
